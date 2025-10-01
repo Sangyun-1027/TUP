1ae6a484ee76b4c1bf052a8334e8ea78e4373070
hangyeoljeong <hangyeoljeong0920@gmail.com>
2025-10-01 13:13:29 +0900

    Delete backend directory

--- backend/TeamMatching1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('save', views.save_user_input),
-    path('apply', views.apply_teamup),
-    path('teams', views.get_matched_teams),
-    path('feedback', views.submit_feedback),
-]
